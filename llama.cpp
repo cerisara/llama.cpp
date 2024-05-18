80a9ea6f7cbc8dbe2307671654125722872b671b
Christophe Cerisara <cerisara@loria.fr>
2024-05-18 22:01:35 +0200

    le nom du tensor n'est pas def dans le fichier ?

--- llama.cpp
+++ llama.cpp
@@ -498,6 +498,7 @@ static const std::map<llm_arch, std::map<llm_tensor, std::string>> LLM_TENSOR_NA
             { LLM_TENSOR_FFN_GATE_EXPS,   "blk.%d.ffn_gate_exps" },
             { LLM_TENSOR_FFN_DOWN_EXPS,   "blk.%d.ffn_down_exps" },
             { LLM_TENSOR_FFN_UP_EXPS,     "blk.%d.ffn_up_exps" },
+            { LLM_TENSOR_ADDACT,          "blk.%d.addact" },
         },
     },
     {
