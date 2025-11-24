f82a318187d90c46e64588bb99a5a7730b91a0e8
xtof <cerisara@loria.fr>
2025-11-24 07:40:48 +0100

    TODO: manage all in python

--- xtof.sh
+++ xtof.sh
@@ -25,7 +25,7 @@ while IFS="" read -r p || [ -n "$p" ]
 do
     rm -rf detlog
     mkdir detlog
-    ./llama-cli --logdir detlog --temp 0 -c 2048 -nkvo -m "$modnom" -p "$p" -n 20 # -fa -ngl 100
+    ./llama-cli --logdir detlog --temp 0.7 -c 2048 -nkvo -m "$modnom" -p "$p" -n 20 # -fa -ngl 100
 done < "$tf"
 
 echo "quit" > pyargs.txt
