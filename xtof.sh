a18765930ebe795b860da4fa34d7752a601704a0
Christophe Cerisara <cerisara@loria.fr>
2026-08-26 11:36:09 +0200

    ok

--- xtof.sh
+++ xtof.sh
@@ -1,8 +1,9 @@
+# cmake -B build
+# cmake --build built --config release
 
 p="/home/xtof/git/researchplm/agentic/q1_oracle.md"
 
 ./build/bin/llama-perplexity -m ~/ggufs/qwen2.5-0.5b-f32.gguf --file "$p" -ngl 99
-exit
 
 ./build/bin/llama-finetune -m ~/ggufs/qwen2.5-0.5b-f32.gguf --file "$p" -t 1 -c 512 -ngl 99 -epochs 1 -o toto.gguf
 
