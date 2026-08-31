379bde036f94676d06a1e0724bfcaefe3bc80eac
Christophe Cerisara <cerisara@loria.fr>
2026-08-31 12:15:02 +0200

    injection de token

--- unittest1.sh
+++ unittest1.sh
@@ -1,29 +1,10 @@
 source ~/envs/transformers/bin/activate
 
 echo 'La capitale de la Belgique est Bruxelles.' > tt
-python ./xllamacpp.py tt > ttgoods.log
-mv tt_activs.npz ttgoods.npz
-
+python ./xllamacpp.py tt | grep PROMPT_TOKENS > ttt
 echo 'La capitale de la Belgique est' > tt
-python ./xllamacpp.py tt > ttbads.log
-mv tt_activs.npz ttbads.npz
-
-python ./xllamacpp.py tt ttbads.npz > ttforce.log
-grep GEN ttbads.log > /tmp/aa
-grep GEN ttforce.log > /tmp/ab
-if diff -q /tmp/aa /tmp/ab > /dev/null; then
-    echo "BADSAME OK"
-else
-    echo "BADSAME KO"
-	diff /tmp/aa /tmp/ab
-fi
-
-python ./xllamacpp.py tt ttgoods.npz > ttforce.log
-grep GEN ttforce.log > /tmp/ab
-if grep -e '^GEN Bruxelles' /tmp/ab; then
-	echo "GOOD OK"
-else
-	echo "GOOD KO"
-	cat /tmp/ab
-fi
+python ./xllamacpp.py tt | grep PROMPT_TOKENS >> ttt
+a=$(tail -1 ttt | wc -c)
+goldtok=$(cut -c$a- ttt | cut -d',' -f1 | cut -c2- | head -1)
+python ./xllamacpp.py tt $goldtok | grep GEN
 
