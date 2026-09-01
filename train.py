a1f1c1ced436b58e8960bff297c9fbd9723c8dd2
Christophe Cerisara <cerisara@loria.fr>
2026-09-01 18:51:23 +0200

    ok

--- train.py
+++ train.py
@@ -21,5 +21,10 @@ print("activations tensors:", len(activs), "last shape:", activs[-1].shape)
 # number of activation vectors should be double the number of prompt tokens
 nactivs = sum(a.shape[0] for a in activs)
 print("activation vectors:", nactivs, "tokens:", len(toks), "expected:", 2 * len(toks))
-assert nactivs == 2 * len(toks), \
-    "activation vectors should be double the prompt tokens"
+assert nactivs == 2 * len(toks), "activation vectors should be double the prompt tokens"
+
+# TODO read all activs by successive pairs, all pairs form the input training
+# dataset for an MLP. The corresponding output is the embedding of the token ID
+# indicated at t+1 (the target token embedding to generate is shifted right wrt
+# the input pair). This embedding can be found in ./detembeds.bin Then write the
+# code to train the MLP (MSE loss)
