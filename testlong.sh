d492bd86b33a8abe171714d76ee92b4c72ded33e
xtofCC <xtof@ada.fr>
2026-09-05 18:55:10 +0200

    OK en fait, il faut juste recompiler

--- testlong.sh
+++ testlong.sh
@@ -1,18 +1,20 @@
 # test if it works with long input splitted into chunks
 
+mod="/home/xtof/ggufs/qwen2.5-0.5b-instruct-q5_k_m.gguf"
+
 source ~/envs/transformers/bin/activate
 
 cat q1_oracle.md > tt
 echo "" >> tt
 echo 'La capitale de la Belgique est Bruxelles.' >> tt
 
-NTOKS=1 python ./xllamacpp.py --prompts tt > repgld
+NTOKS=1 python ./xllamacpp.py --model "$mod" --prompts tt > repgld
 rm -f tt_activs.npz
 
 cat q1_oracle.md > tt
 echo "" >> tt
 echo 'La capitale de la Belgique est' >> tt
-NTOKS=5 python ./xllamacpp.py --prompts tt > repbad
+NTOKS=5 python ./xllamacpp.py --model "$mod" --prompts tt > repbad
 mv tt_activs.npz actbad.npz
 a=$(cat repbad | grep PROMPT_TOKENS | wc -w)
 # contient le nb de tokens+1; l'index du token genere suivant = nb de tokens
@@ -20,6 +22,6 @@ a=$(cat repbad | grep PROMPT_TOKENS | wc -w)
  
 goldtok=$(cat repgld | grep PROMPT_TOKENS | cut -c16- | cut -d',' -f$a)
 echo "gold token $goldtok $a"
-python ./xllamacpp.py --prompts tt --inject_token $goldtok > repfix
+python ./xllamacpp.py --model "$mod" --prompts tt --inject_token $goldtok > repfix
 cat repfix | grep GEN
 
--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
 # source ~/envs/transformers/bin/activate
 source ~/envs/hf/bin/activate
 
+mod="/home/xtof/ggufs/qwen2.5-0.5b-instruct-q5_k_m.gguf"
+
 rm q1_oracle_activs.npz
-LOGITS_ALL=1 NTOKS=1 python ./xllamacpp.py --prompts ~/git/researchplm/agentic/q1_oracle.md > q1.log
+LOGITS_ALL=1 NTOKS=1 python ./xllamacpp.py --model "$mod" --prompts ~/git/researchplm/agentic/q1_oracle.md > q1.log
 
 
