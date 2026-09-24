a6644302d4fe139d2f73b7e846fda2d32e96c9a6
xtof <cerisara@loria.fr>
2026-09-24 07:21:34 +0200

    ok

--- train.sh
+++ train.sh
@@ -6,4 +6,4 @@ mod="/home/xtof/ggufs/qwen2.5-0.5b-instruct-q5_k_m.gguf"
 rm q1_oracle_activs.npz
 LOGITS_ALL=1 NTOKS=1 python ./xllamacpp.py --model "$mod" --prompts ~/git/researchplm/agentic/q1_oracle.md > q1.log
 
-
+# TODO: on-policy distillation: oracle prompt + rollout + forward to get acts + train.py
